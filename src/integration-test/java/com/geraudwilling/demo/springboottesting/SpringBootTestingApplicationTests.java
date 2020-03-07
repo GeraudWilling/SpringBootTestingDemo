7f20edb9461e59ed1f79a1f42c921cad7d001ebc
GéraudWilling <geraudwilling@hotmail.fr>
2020-03-07 01:26:29 +0100

    [FEAT] Adding configuration for integration test and updating README.md

---
+++ src/integration-test/java/com/geraudwilling/demo/springboottesting/SpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraudwilling.demo.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingApplicationIntegrationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
