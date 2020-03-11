267beb385fa306f3765c9f62a6f77c201527dfe4
GéraudWilling <geraudwilling@hotmail.fr>
2020-03-12 00:57:21 +0100

    [FIX] Adding static context initializer to register datasource with PostgresTestContainer

--- src/integration-test/resources/add_employees.sql
+++ src/integration-test/resources/add_employees.sql
@@ -1,6 +1,14 @@
-INSERT INTO Employee(id,employeeName,employeeSalary,employeeAge,profileImage)
-    VALUES(1,"Toto", 2000, 25, '');
-INSERT INTO Employee(id,employeeName,employeeSalary,employeeAge,profileImage)
-    VALUES(2,"Titi", 2000, 52, '');
-INSERT INTO Employee(id,employeeName,employeeSalary,employeeAge,profileImage)
-    VALUES(3,"Bubu", 0, 0, '');
+CREATE TABLE Employee (
+   id  integer PRIMARY KEY,
+   employee_name  varchar NOT NULL,
+   employee_salary  integer,
+   employee_age  integer,
+   profile_image  varchar
+) ;
+
+INSERT INTO Employee(id,employee_name,employee_salary,employee_age,profile_image)
+    VALUES(1,'Toto', 2000, 25, '');
+INSERT INTO Employee(id,employee_name,employee_salary,employee_age,profile_image)
+    VALUES(2,'Titi', 2000, 52, '');
+INSERT INTO Employee(id,employee_name,employee_salary,employee_age,profile_image)
+    VALUES(3,'Bubu', 0, 0, '');
--- src/main/java/com/geraudwilling/demo/springboottesting/entity/Employee.java
+++ src/main/java/com/geraudwilling/demo/springboottesting/entity/Employee.java
@@ -1,8 +1,10 @@
 package com.geraudwilling.demo.springboottesting.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
@@ -12,20 +14,26 @@ import javax.validation.constraints.NotEmpty;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Employee {
     @Id
     Long id;
 
     @JsonProperty("employee_name")
     @NotEmpty()
+    @Column(name = "employee_name", nullable = false)
     String employeeName;
 
+
     @JsonProperty("employee_salary")
+    @Column(name = "employee_salary")
     int employeeSalary;
 
     @JsonProperty("employee_age")
+    @Column(name = "employee_age")
     int employeeAge;
 
     @JsonProperty("profile_image")
+    @Column(name = "profile_image")
     String profileImage;
 }
